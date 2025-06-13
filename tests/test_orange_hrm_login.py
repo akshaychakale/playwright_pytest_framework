bdac3a2e752d2064b07b81d6e15637fdb8ac4c60
akshaychakale <akshaychakale@gmail.com>
2025-06-13 11:56:57 +0530

    updates config files

--- tests/test_orange_hrm_login.py
+++ tests/test_orange_hrm_login.py
@@ -1,8 +1,11 @@
 from pages.orange_hrm_login_page import orange_hrm_login_page
+from utils.config import get_orangeHrm_credentials
 
 
 def test_orangehrm_login(page):
+    creds=get_orangeHrm_credentials()
     login_page = orange_hrm_login_page(page)
     login_page.load()
-    login_page.login("Admin","admin123")
+    login_page.login(creds["username"],creds["password"])
     login_page.submit_button
+
--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,18 @@
 import yaml
 import os
 
+
 def load_config():
     config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'config.yaml')
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
+
 def get_amazon_credentials():
     config = load_config()
     return config.get("amazon", {})
+
+
+def get_orangeHrm_credentials():
+    config = load_config()
+    return config.get("orangeHrm", {})
